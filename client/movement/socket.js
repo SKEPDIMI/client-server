a1cfc5544a9894c1c6f6706a0c793ca3836fa6cb
John S <diaz.john312@gmail.com>
2018-10-31 19:49:57 -0400

    added client/server communication

--- client/movement/socket.js
+++ client/movement/socket.js
@@ -1,24 +1,34 @@
-var Client = {};
-
-Client.socket = io('/movement');
+var Client = {
+  socket: io('/movement'),
+  id: 0,
+};
 
 Client.joinGame = function() {
+  Client.id = this.socket.id;
+
   Client.socket.emit('join_game');
 }
 
-Client.socket.on('users_data', function(users_data) {
-  // for all users in the server users data, if it exists update them, if not, add them
-  for (id in users_data) {
-    user = users_data[id]
-    if (users_list.hasOwnProperty(id)) {
-      updatePlayer(id, user)
+var t1 = Date.now();
+var serverDeltaTime = 0;
+
+Client.socket.on('usersPool', function(usersPool) {
+  var serverDeltaTime = Date.now() - t1;
+  t1 += serverDeltaTime;
+
+  for (var id in usersPool) {
+    var data = usersPool[id];
+    if(users_list[id]) {
+      // inerpolate their movementQueue!
+      users_list[id].movementQueue = data.movementQueue;
     } else {
-      addPlayer(id, user)
+      // add new user
+      addPlayer(id, data);
     }
   }
 
   for (id in users_list) { // Will get rid of users in the game if they are not in the server users_data object
-    if (!users_data.hasOwnProperty(id)) {
+    if (!usersPool.hasOwnProperty(id)) {
       removePlayer(id)
     }
   }
@@ -39,22 +49,27 @@ $(document).on('keydown', function(event) {
 
   switch(event.which){
     case 87:
-      Client.socket.emit('move', 'up')
+      Client.socket.emit('move', 'up');
+      currentUser.animateMovement(true, 'up');
       break
     case 65:
-      Client.socket.emit('move', 'left')
+      Client.socket.emit('move', 'left');
+      currentUser.animateMovement(true, 'left');
       break
     case 83:
-      Client.socket.emit('move', 'down')
+      Client.socket.emit('move', 'down');
+      currentUser.animateMovement(true, 'down');
       break
     case 68:
-      Client.socket.emit('move', 'right')
+      Client.socket.emit('move', 'right');
+      currentUser.animateMovement(true, 'right');
       break
   }
 });
 
 $(document).on('keyup', function() {
-  let {x, y} = users_list[Client.socket.id];
-  Client.socket.emit('move_stop', {x, y});
+  Client.socket.emit('move_stop');
+  currentUser.animateMovement(false, currentUser.direction);
+
   allowed = true
 });
