4553808bd1924a43fa3bf5f386c6cf3ce9e84932
John S <diaz.john312@gmail.com>
2018-10-31 19:49:18 -0400

    added game loops

--- client/movement/script.js
+++ client/movement/script.js
@@ -14,12 +14,17 @@ var config = {
   },
   scene: {
       preload: preload,
-      create: create
-  }
+      create: create,
+      update: update
+  },
 };
 
 var game = new Phaser.Game(config);
+
 var _this;
+var t1;
+var updateInterval = 1000;
+var currentUser = null;
 
 function preload ()
 {
@@ -29,96 +34,56 @@ function preload ()
         '../public/assets/sprites.png',
         { frameWidth: 64, frameHeight: 96 }
     );
+    t1 = Date.now();
 }
 
 function create ()
 {
     Client.joinGame();
+    Player.addAnimations();
+
     this.add.image(0, 0, 'sky').setOrigin(0, 0);
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 7 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 8, end: 15 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'up',
-        frames: this.anims.generateFrameNumbers('dude', { start: 16, end: 23 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'down',
-        frames: this.anims.generateFrameNumbers('dude', { start: 24, end: 31 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'idle',
-        frames: [ {key: 'dude', frame: 25} ],
-        frameRate: 10,
-        repeat: 1
-    });
 }
 
-function addPlayer(user_id, data) { // ADDED USER, SO UPDATED
-    users_list[user_id] = _this.physics.add.sprite(data.x, data.y, 'dude');
-    player = users_list[user_id];
+var t2 = Date.now();
 
-    player.setBounce(0.1);
-    player.setCollideWorldBounds(true);
-}
+function update() {
+    // update whenever we receive update from server
+    var delta = Date.now() - t2;
+    if (delta >= serverDeltaTime) {
+        t2 = Date.now();
 
-function updatePlayer(user_id, data) {
-    player = users_list[user_id];
+        for(let id in users_list) {
+            var player = users_list[id];
+    
+            if (player.movementQueue.length == 0) {
+                continue
+            }
+    
+            // grab the last movement stack
+            movementStack = player.movementQueue.shift();
+            /*if (!movementStack.moving) {
+                continue
+            }*/
 
-    if (data.moving) {
-        switch (data.moving) {
-            case 'left':
-                player.setVelocityX(-200);
-                player.anims.play('left', true);
-                break;
-            case 'right':
-                player.setVelocityX(200);
-                player.anims.play('right', true);
-                break;
-            case 'up':
-                player.setVelocityY(-200);
-                player.anims.play('up', true);
-                break;
-            case 'down':
-                player.setVelocityY(200);
-                player.anims.play('down', true);
-                break;
+            player.update(movementStack);
         }
-    } else {
-        var { x, y } = data
-        player.x = x
-        player.y = y
-        player.setVelocityX(0);
-        player.setVelocityY(0);
-        player.anims.play('idle');
     }
 }
 
-function removePlayer(id) {
-    users_list[id].destroy();
-    delete users_list[id];
+function addPlayer(user_id, data) { // ADDED USER, SO UPDATED
+    users_list[user_id] = new Player(data);
+
+    if (user_id == Client.id) {
+        currentUser = users_list[user_id];
+    }
 }
 
-function userStop (id) {
-    player = users_list[id];
-    player.setVelocityX(0);
-    player.setVelocityY(0);
-    player.anims.play('idle');
+function updatePlayer(user_id, data) {
+    users_list[user_id].update(data);
+}
+
+function removePlayer(id) {
+    users_list[id].sprite.destroy();
+    delete users_list[id];
 }
