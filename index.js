e9a596238f6c3c18cb196cf7f820b57542e7479a
John S <diaz.john312@gmail.com>
2018-11-10 22:49:48 -0500

    added cors, changed port number

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
 var server = require('http').createServer(app);  
 var io = require('socket.io')(server, { reconnection: false });
 
 require('./lib/routes/movement')(io);
 
+app.use(cors());
 app.use(express.static(__dirname + '/client'));  
 app.get('/', function(req, res) { 
   res.sendFile(__dirname + '/client/index.html');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log('Listening at PORT ' + PORT))
