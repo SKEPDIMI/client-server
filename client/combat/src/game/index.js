faedbdd9601000d4fb0f76ee78b09f94b745bc98
John S <diaz.john312@gmail.com>
2018-11-11 18:02:19 -0500

    added binding for game

--- client/combat/src/game/index.js
+++ client/combat/src/game/index.js
@@ -1,65 +1,82 @@
-import Phaser from 'phaser'
+import Phaser from 'phaser';
+
+import helpers from './helpers';
+
 import loadAssets from './preload/loadAssets';
 import initiateEntities from './preload/initiateEntities';
 
-const PLAYERS = {
-  1: {
-    name: 'skepdimi',
-    type: 'dwarf',
-  }
-}
+export default (store) => {
+  let state = {}
+  store.subscribe(() => {
+    state = store.getState()
+  });
 
-// access game instance from everywhere
-var gameInstance;
+  // access game instance from everywhere
+  var gameInstance;
 
-var config = {
-  type: Phaser.AUTO,
-  width: window.innerWidth,
-  height: window.innerHeight,
-  parent: 'root',
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 200 }
-    }
-  },
-  scene: {
-    preload,
-    create,
-    update
-  }
-};
-
-function preload ()
-{
-  gameInstance = this;
-
-  loadAssets(gameInstance);
-}
- 
-function create ()
-{
-  initiateEntities(gameInstance);
-  
-  gameInstance.bg = gameInstance.add.image(window.innerWidth/2, window.innerHeight/2, 'bg')
-  gameInstance.bg.setDisplaySize(window.innerWidth, window.innerHeight);
-
-  Object.values(PLAYERS).forEach(data => {
-    let player = this.entities['dwarf'](gameInstance,
-      data.name,
-      {
-        x: window.innerWidth/4,
-        y: window.innerHeight*(4/7)
+  var config = {
+    type: Phaser.AUTO,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    parent: 'root',
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: { y: 200 }
       }
+    },
+    scene: {
+      preload,
+      create,
+      update
+    }
+  };
+
+  function preload ()
+  {
+    gameInstance = this;
+
+    loadAssets(gameInstance);
+  }
+  
+  function create ()
+  {
+    initiateEntities(gameInstance);
+    
+    gameInstance.bg = gameInstance.add.image(window.innerWidth/2, window.innerHeight/2, 'bg')
+    gameInstance.bg.setDisplaySize(window.innerWidth, window.innerHeight);
+
+    /* Object.values(PLAYERS).forEach(data => {
+      let player = this.entities['dwarf'](gameInstance,
+        data.name,
+        
+      );
+    })*/
+  }
+
+  function update ()
+  {
+    if(!this.currentPlayer && state.currentPlayer) {
+      this.currentPlayer = state.currentPlayer
+      spawn(this.currentPlayer);
+    }
+  }
+
+  function spawn({ entity, name }) {
+    let selected = gameInstance.entities[entity.id];
+
+    if (!selected) {
+      console.error('UNKNOWN ENTITY: ', entity.id);
+    }
+
+    new selected(
+      gameInstance,
+      name,
+      helpers.coordinatesForEntityNumber(
+        Object.keys(state.players).length
+      )
     );
-  })
+  }
+
+  var game = new Phaser.Game(config);
 }
-
-function update ()
-{
-
-}
-
-var game = new Phaser.Game(config);
-
-export default game
