0e542481292b4fc390079ab26d2c683bef5521ca
John S <diaz.john312@gmail.com>
2018-11-11 20:18:54 -0500

    changed asset directories

--- client/combat/src/game/preload/loadAssets.js
+++ client/combat/src/game/preload/loadAssets.js
@@ -1,6 +1,6 @@
 export default (gameInstance) => {
   gameInstance.load.setBaseURL('http://localhost:5000')
   
-  gameInstance.load.image('bg', 'public/assets/bg-forest.png');
-  gameInstance.load.spritesheet('dwarf', 'public/assets/dwarf.png', { frameWidth: 32, frameHeight: 40 });
+  gameInstance.load.image('bg', 'public/assets/img/bg-forest.png');
+  gameInstance.load.spritesheet('dwarf', 'public/assets/img/characters/dwarf.png', { frameWidth: 32, frameHeight: 40 });
 }
